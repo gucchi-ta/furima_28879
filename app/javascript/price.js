547bf91549514c3654658a0734cafc4da7340070
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-08-31 21:29:17 +0900

    jsファイルにコメント追加

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,14 +1,18 @@
+//price関数を定義
 function price() {
+  //要素を取得
   const item_price = document.getElementById("item-price")
   const add_tax_price = document.getElementById("add-tax-price")
   const profit = document.getElementById("profit")
 
-  item_price.addEventListener('keyup', () => {
+  //イベント発火時の関数を定義
+  item_price.addEventListener('keyup', () => {    //keyupイベント発火(キーが離れた時)で以下関数を実行
     let tax_value = item_price.value / 10;
-    tax_value = parseInt(tax_value);          //小数点以下を切り捨て
-    add_tax_price.innerHTML = tax_value;      //innerHTML関数を使い計算結果を出力
+    tax_value = parseInt(tax_value);              //小数点以下を切り捨て
+    add_tax_price.innerHTML = tax_value;          //innerHTML関数を使い計算結果を出力
     let profit_value = item_price.value - tax_value;
     profit.innerHTML = profit_value;
   });
 }
+//画面ロード時に定義したplice関数を実行
 window.addEventListener("load", price);
