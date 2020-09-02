b6750bbc8b3e0f1d00b6684dc60031ff42357000
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-09-02 21:22:18 +0900

    rubokopの実行と修正
    rubocopの実行の際
    bundle exec rubocop -lをしていたが
    bundle exec rubocop -aを使い修正後
    bundle exec rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,39 +1,39 @@
 class ItemsController < ApplicationController
-   before_action :move_to_index, except: [:index, :show]
-   
-   def index
-      @items = Item.includes(:user).order("created_at DESC")
-      @orders = Order.includes(:user, :item)
-   end
-   
-   def new
-      @item = Item.new
-   end
-   
-   def create
-      @item = Item.new(item_params)
-      if @item.save
-         redirect_to root_path
-      else
-         render :new
-      end
-   end
+  before_action :move_to_index, except: [:index, :show]
 
-   def show
-      @item = Item.find(params[:id])
-      @orders = Order.includes(:user, :item)
-   end
+  def index
+    @items = Item.includes(:user).order('created_at DESC')
+    @orders = Order.includes(:user, :item)
+  end
 
-   private
+  def new
+    @item = Item.new
+  end
 
-   def item_params
-      params.require(:item).permit(:image, :item_name, :text, :category_id, :product_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
-   end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
-   def move_to_index
-      unless user_signed_in?
-         redirect_to action: :index
-      end
-   end
+  def show
+    @item = Item.find(params[:id])
+    @orders = Order.includes(:user, :item)
+  end
 
+  private
+
+  def item_params
+    params.require(:item)
+          .permit(:image, :item_name, :text, :category_id, :product_status_id,
+                  :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price)
+          .merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,22 +8,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # 空の投稿を保存できないようにする
-  validates :nickname,:birthday, presence: true
-  
+  validates :nickname, :birthday, presence: true
+
   # パスワードのバリデーション
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: PASSWORD_REGEX}, length: { minimum: 6 }
+  validates :password, format: { with: PASSWORD_REGEX }, length: { minimum: 6 }
 
-  #ユーザー本名のバリデーション
+  # ユーザー本名のバリデーション
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
     validates :first_name
     validates :last_name
   end
 
-  #ユーザー本名フリガナのバリデーション
+  # ユーザー本名フリガナのバリデーション
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
     validates :first_kana
     validates :last_kana
   end
-
 end
