b637a23bf395aa489a1814bd5911b5388892115a
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-09-02 14:44:20 +0900

    ビューファイルの配置/詳細画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,11 @@
 class ItemsController < ApplicationController
+   before_action :move_to_index, except: [:index, :show]
    
    def index
       @items = Item.includes(:user).order("created_at DESC")
       @orders = Order.includes(:user, :item)
    end
    
-   def move_to_index
-      unless user_signed_in?
-         redirect_to action: :index
-      end
-   end
-
    def new
       @item = Item.new
    end
@@ -28,6 +23,12 @@ class ItemsController < ApplicationController
 
    def item_params
       params.require(:item).permit(:image, :item_name, :text, :category_id, :product_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+   end
+
+   def move_to_index
+      unless user_signed_in?
+         redirect_to action: :index
+      end
    end
 
 end
