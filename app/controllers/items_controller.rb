ef5eebfb4ec8b005b53dcc34799815f90903ead2
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-09-03 19:08:05 +0900

    rubocopの実行/修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update] 
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
