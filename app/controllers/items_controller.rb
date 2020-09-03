47b3126a6c37e16a2a012a2b46f106f31fd112a6
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-09-03 22:54:26 +0900

    バリーデーション条件分岐の表記を変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,8 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if @item.valid?
+      @item.save
       redirect_to root_path
     else
       render :new
@@ -28,7 +29,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.update(item_params)
+    if @item.valid?
+      @item.update(item_params)
       redirect_to root_path
     else
       render :edit
@@ -36,7 +38,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if @item.valid?
+      @item.destroy
       redirect_to root_path
     else
       render :show
