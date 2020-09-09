52c60583ce472241176222e3c97297f36351f5ed
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-09-09 20:13:49 +0900

    orderコントローラーへのルーティングをネスト/それに伴いparamsのキーの記述を[:id]から[:item_id]に変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,13 @@ class OrdersController < ApplicationController
   before_action :move_to_root, only: :index
   def index
     @order = OrderAddress.new
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    binding.pry
     @order = OrderAddress.new(order_params)
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
     if @order.valid?
       @order.save
       pay_item
@@ -22,7 +23,7 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone)
-          .merge(user_id: current_user.id, token: params[:token], item_id: params[:id])
+          .merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
 
   def pay_item
@@ -37,7 +38,9 @@ class OrdersController < ApplicationController
 
   def move_to_root
     @orders = Order.includes(:user, :item)
-    @user = Item.find(params[:id]).user_id
-    redirect_to root_path if @user == current_user.id || @orders.where(item_id: params[:id]).present?
+    @user = Item.find(params[:item_id]).user_id
+    if @user == current_user.id || @orders.where(item_id: params[:item_id]).present?
+      redirect_to root_path 
+    end
   end
 end
