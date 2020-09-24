dc735fc05d06863cade243a5add7cbd91834554b
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-09-25 05:17:07 +0900

    マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'messages/new'
   devise_for :users
+  resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items do
