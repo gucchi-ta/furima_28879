68e97f35220444e02ff53e0a997db5cd3af0d6af
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-09-27 04:32:18 +0900

    action_cableの記述変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://54.199.124.52/' ]
+
+  config.action_cable.url = [ 'http://54.199.124.52/' 
+  config.action_cable.allowed_request_origins = [ 'http://54.199.124.52/', http:\/\/54.199.124.52.*/ ]
+  ActionCable.server.config.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
