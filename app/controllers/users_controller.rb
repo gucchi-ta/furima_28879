1d2cdb998e3593c4c0515e90d2351ff93ada40b9
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-09-27 04:01:12 +0900

    ユーザーページの制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @nickname = current_user.nickname
     @items = current_user.items.order('created_at DESC')
