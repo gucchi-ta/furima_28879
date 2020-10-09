e075beea679a3d73404a2212aee4a5d3eed5ed8c
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-09 16:53:10 +0900

    購入済み商品一覧を降順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @nickname = current_user.nickname
     @items = current_user.items.order('created_at DESC')
     @p_orders = Order.all.where(user_id: current_user.id).select('item_id')
-    @p_items = Item.all.where(id: @p_orders)
+    @p_items = Item.all.order('created_at DESC').where(id: @p_orders)
     @good_items = Item.all.order('created_at DESC').where('checked LIKE ? ', true)
     @orders = Order.includes(:user, :item)
   end
