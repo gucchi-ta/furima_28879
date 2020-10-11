1cd7d3994114361902c88fb29aeec5db4f8886d7
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-11 19:47:07 +0900

    メッセージ バリデーション

--- db/migrate/20200924163053_create_messages.rb
+++ db/migrate/20200924163053_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.text :message
+      t.text :message, null: false
       t.references :item , null: false, foreign_key: true
       t.references :user , null: false, foreign_key: true
       t.timestamps
