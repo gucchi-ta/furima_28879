31825cd6bc13b41c17be9d49b479e3575dc2e426
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-12 21:02:02 +0900

    コメントにニックネーム・投稿日時を表示

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -10,17 +10,27 @@ consumer.subscriptions.create("MessageChannel", {
   },
 
   received(data) {
-    console.log("a")
-    const html = `<p>${data.content.message}</p>`;
+    const userNickname = document.getElementById('user-nickname').textContent;
+    const today = new Date();
+    const message = data.content.message.replace(/\r\n/g, "<br>").replace(/(\n|\r)/g, "<br>");
+    const html = 
+      `<div class="message-border">
+        <p class="message-nickname">
+          ${userNickname}
+          <a class="message-date">
+            ${today.getFullYear()}-${(today.getMonth()+1)}-${today.getDate()}&nbsp;${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}&nbsp;UTC
+          </a>
+        </p>
+        <p class="message-mesage">
+          ${message}
+        </p>
+      </div>`;
     console.log(html)
     const messages = document.getElementById('messages');
-    console.log(messages)
     const newMessage = document.getElementById('message_message');
-    console.log(newMessage)
     messages.insertAdjacentHTML('afterbegin', html);
     newMessage.value='';
     const messageBtn = document.getElementById('message-btn');
     messageBtn.disabled = false;
-    console.log("z")
   }
 });
