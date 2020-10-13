d85d592ab5af92500c5327c7ffa2f0290be675c9
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-14 05:34:18 +0900

    タグフォームの増減の更新

--- app/javascript/add_tag.js
+++ app/javascript/add_tag.js
@@ -3,32 +3,37 @@ const { htmlPrefilter } = require("jquery");
 
 window.addEventListener("load", (e) => {
   const newElement = document.getElementById("new-tag");
-  const tagFormElement = document.getElementById("tag-form");
+  let tagFormElements = document.getElementsByClassName("tag-form");
   const plusImageElement = document.getElementById("plus-image");
   const itemTagElement = document.getElementById("item-tag");
   const sellBtnElement = document.getElementsByClassName("sell-btn");
+  let minusImageElements = document.getElementsByClassName("minus-image");
   
-  console.log(newElement);
+  console.log(tagFormElements);
 
   plusImageElement.addEventListener("click", (e) => {
-    const HTML = `<div id="tag-form"> 
-                    <textarea class="items-tag" id="item-tag" maxlength="20" name="item[tag][tag_name]"></textarea>
-                    <img id="minus-image" src="/assets/minus_image-6d029aef624144816d94d6cd1c062954a4c90c78a499b4d9694cfb63b48ec0aa.png" width="15" height="15">
+    const HTML = `<div id="tag-form" class="tag-form"> 
+                    <textarea class="item-tag" id="item-tag" maxlength="20" name="item[tag][tag_name]"></textarea>
+                    <img id="minus-image" class="minus-image mouseon_cursor" src="/assets/minus_image-6d029aef624144816d94d6cd1c062954a4c90c78a499b4d9694cfb63b48ec0aa.png" width="15" height="15">
                   </div>`
     console.log(HTML)
-    if(HTML.value){}else{
+    // if(HTML.value){}else{
       console.log("4");
       newElement.insertAdjacentHTML("beforeend", HTML);
-    };
-    console.log(itemTagElement)
-    const formData = new FormData(itemTagElement);
-    console.log(formData)
-    const tag = {
-      tag_name: formData.get("tag_name")
-    };
+    // };
+    console.log(itemTagElement);
+    const formData = itemTagElement.value;
+    console.log(formData);
+    minusImageElements = document.getElementsByClassName("minus-image");
+    tagFormElements = document.getElementsByClassName("tag-form");
+    console.log(tagFormElements);
   });
-  const minusImageElement = document.getElementById("minus-image");
-  minusImageElement.addEventListener("click", (e) => {
-    tagFormElement.remove();
-  });
+  for(var i=0; i < minusImageElements.length; i++){
+    minusImageElements[i].addEventListener("click", (e) => {
+      console.log(minusImageElements[i]);
+      console.log(tagFormElements[i])
+      tagFormElements[i].remove();
+      console.log(tagFormElements)
+    });
+  }
 });
