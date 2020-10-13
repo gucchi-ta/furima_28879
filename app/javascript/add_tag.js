efe6986a8e262011308cfb0ef195f57898355bf0
gucchi-ta <ta.yamaguchi.1225@gmail.com>
2020-10-14 08:55:21 +0900

    タグの増減を更新

--- app/javascript/add_tag.js
+++ app/javascript/add_tag.js
@@ -1,39 +1,55 @@
 // タグの増減のためのjs(未完全)
-const { htmlPrefilter } = require("jquery");
+require("jquery");
 
-window.addEventListener("load", (e) => {
-  const newElement = document.getElementById("new-tag");
+function addTagFunction() {
+  
+  // 要素の取得
+  const newTagElement = document.getElementById("new-tag");
   let tagFormElements = document.getElementsByClassName("tag-form");
   const plusImageElement = document.getElementById("plus-image");
-  const itemTagElement = document.getElementById("item-tag");
+  let itemTagElements = document.getElementsByClassName("item-tag");
   const sellBtnElement = document.getElementsByClassName("sell-btn");
   let minusImageElements = document.getElementsByClassName("minus-image");
-  
-  console.log(tagFormElements);
+  // /要素の取得
 
+  // -ボタンクリック時
+  let num = minusImageElements.length-1
+  console.log(num);
+
+  if(num > 0){
+    minusImageElements[num].addEventListener("click", (e) => {
+      tagFormElements[num].remove();
+      num = minusImageElements.length-1
+      console.log(num);
+    });
+  }
+  // /-ボタンクリック時
+
+
+  // +ボタンクリック時に呼び出したaddTagFunction();の+ボタンクリックイベントが重複して実行されないように
+  if (plusImageElement.getAttribute("data-load") != null) {
+    return null;
+  }
+  plusImageElement.setAttribute("data-load", "true");
+  // +ボタンクリック時に呼び出したaddTagFunction();の+ボタンクリックイベントが重複して実行されないように
+
+
+  // +ボタンクリック時
   plusImageElement.addEventListener("click", (e) => {
+    // 追加するフォームのHTML定義
     const HTML = `<div id="tag-form" class="tag-form"> 
                     <textarea class="item-tag" id="item-tag" maxlength="20" name="item[tag][tag_name]"></textarea>
                     <img id="minus-image" class="minus-image mouseon_cursor" src="/assets/minus_image-6d029aef624144816d94d6cd1c062954a4c90c78a499b4d9694cfb63b48ec0aa.png" width="15" height="15">
                   </div>`
-    console.log(HTML)
-    // if(HTML.value){}else{
-      console.log("4");
-      newElement.insertAdjacentHTML("beforeend", HTML);
-    // };
-    console.log(itemTagElement);
-    const formData = itemTagElement.value;
-    console.log(formData);
-    minusImageElements = document.getElementsByClassName("minus-image");
-    tagFormElements = document.getElementsByClassName("tag-form");
-    console.log(tagFormElements);
+    // フォームの追加
+    newTagElement.insertAdjacentHTML("beforeend", HTML);
+
+    // 追加した要素を取得, -ボタンイベントを再読み込みのため
+    addTagFunction();
+    // /追加した要素を取得, -ボタンイベントを再読み込みのため
   });
-  for(var i=0; i < minusImageElements.length; i++){
-    minusImageElements[i].addEventListener("click", (e) => {
-      console.log(minusImageElements[i]);
-      console.log(tagFormElements[i])
-      tagFormElements[i].remove();
-      console.log(tagFormElements)
-    });
-  }
-});
+  // /+ボタンクリック時
+
+};
+
+window.addEventListener("load", addTagFunction);
